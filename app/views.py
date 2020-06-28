272189b960167e4883e5abb261fa706f2c294cfa
Daniel-Itzul <mayan.bridge.consulting@gmail.com>
2020-06-28 20:13:25 +0800

    added templates and interaction

--- app/views.py
+++ app/views.py
@@ -1,10 +1,15 @@
 from app import app
 from flask import render_template
+from flask import request, redirect
 
 @app.route('/')
 def index():
     return render_template("public/index.html")
 
-@app.route('/main')
+@app.route('/main', methods=["GET", "POST"])
 def main():
-    return render_template("main.html")
+    if request.method == "POST":
+        req = request.form
+        print(req)
+        return redirect(req["target_page"])
+    return render_template("public/main.html")
