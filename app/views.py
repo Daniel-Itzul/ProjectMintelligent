fb25c71bef2d25fc6d74337d03fb2891676297aa
Daniel-Itzul <mayan.bridge.consulting@gmail.com>
2020-07-05 17:01:17 +0800

    integrated engine

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
 from app import app
 from flask import render_template
 from flask import request, redirect
+import scrapmaster
+import json
+
+extractDef = {
+                "type":"1",
+                "url":"",
+                "elementSet":{"etag":"li","eclass":"zg-item-immersion"},
+                "elementDef":[
+                    {"elementName":"Rank","elementTag":"span","elementClass":"zg-bexadge-text","extractType":"0","elementExtract":"text"},
+                    {"elementName":"Url","elementTag":"a","elementClass":"a-link-normal","extractType":"1","elementExtract":"href"},
+                    {"elementName":"Description","elementTag":"div","elementClass":"p13n-sc-truncate","extractType":"0","elementExtract":"text"},
+                    {"elementName":"Price","elementTag":"span","elementClass":"p13n-sc-price","extractType":"0","elementExtract":"text"},
+                    {"elementName":"Stars","elementTag":"span","elementClass":"a-icon-alt","extractType":"0","elementExtract":"text"}
+                ]
+            }
 
 @app.route('/')
 def index():
@@ -10,6 +25,7 @@ def index():
 def main():
     if request.method == "POST":
         req = request.form
-        print(req)
-        return redirect(req["target_page"])
+        extractDef["url"] = req["target_page"]
+        grid_request = scrapmaster.scrapGrid(extractDef)
+        print(grid_request)
     return render_template("public/main.html")
