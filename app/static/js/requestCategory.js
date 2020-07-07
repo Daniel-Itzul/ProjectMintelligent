7fa1f1e4f962df704bf57c1626179a1e59c3699f
Daniel-Itzul <mayan.bridge.consulting@gmail.com>
2020-07-07 16:13:05 +0800

    added javascript

---
+++ app/static/js/requestCategory.js
@@ -0,0 +1,36 @@
+function submit_request() {
+    var page = document.getElementById("target_page");
+    var entry = {
+        type:"1",
+        url:page.value,
+        elementSet:{etag:"li",eclass:"zg-item-immersion"},
+        elementDef:[
+            {elementName:"Rank",elementTag:"span",elementClass:"zg-bexadge-text",extractType:"0",elementExtract:"text"},
+            {elementName:"Url",elementTag:"a",elementClass:"a-link-normal",extractType:"1",elementExtract:"href"},
+            {elementName:"Description",elementTag:"div",elementClass:"p13n-sc-truncate",extractType:"0",elementExtract:"text"},
+            {elementName:"Price",elementTag:"span",elementClass:"p13n-sc-price",extractType:"0",elementExtract:"text"},
+            {elementName:"Stars",elementTag:"span",elementClass:"a-icon-alt",extractType:"0",elementExtract:"text"}
+        ]
+    };
+    fetch(`${window.origin}/main/request-category`, {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify(entry),
+        cache: "no-cache",
+        headers: new Headers({
+          "content-type": "application/json"
+        })
+      })
+        .then(function (response) {
+          if (response.status !== 200) {
+            console.log(`Looks like there was a problem. Status code: ${response.status}`);
+            return;
+          }
+          response.json().then(function (data) {
+            console.log(data);
+          });
+        })
+        .catch(function (error) {
+          console.log("Fetch error: " + error);
+        });
+}
--- app/views.py
+++ app/views.py
@@ -1,31 +1,19 @@
 from app import app
 from flask import render_template
-from flask import request, redirect
+from flask import request, redirect, jsonify, make_response
 import scrapmaster
 import json
-
-extractDef = {
-                "type":"1",
-                "url":"",
-                "elementSet":{"etag":"li","eclass":"zg-item-immersion"},
-                "elementDef":[
-                    {"elementName":"Rank","elementTag":"span","elementClass":"zg-bexadge-text","extractType":"0","elementExtract":"text"},
-                    {"elementName":"Url","elementTag":"a","elementClass":"a-link-normal","extractType":"1","elementExtract":"href"},
-                    {"elementName":"Description","elementTag":"div","elementClass":"p13n-sc-truncate","extractType":"0","elementExtract":"text"},
-                    {"elementName":"Price","elementTag":"span","elementClass":"p13n-sc-price","extractType":"0","elementExtract":"text"},
-                    {"elementName":"Stars","elementTag":"span","elementClass":"a-icon-alt","extractType":"0","elementExtract":"text"}
-                ]
-            }
 
 @app.route('/')
 def index():
     return render_template("public/index.html")
 
-@app.route('/main', methods=["GET", "POST"])
+@app.route('/main')
 def main():
-    if request.method == "POST":
-        req = request.form
-        extractDef["url"] = req["target_page"]
-        grid_request = scrapmaster.scrapGrid(extractDef)
-        print(grid_request)
     return render_template("public/main.html")
+
+@app.route("/main/request-category", methods=["POST"])
+def create_entry():
+    req = scrapmaster.scrapGrid(request.get_json())
+    res = make_response(req, 200)
+    return res
